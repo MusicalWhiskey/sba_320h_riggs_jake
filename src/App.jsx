1ab113ececfca1327bbb31613e42da164387e7eb
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-11-15 23:32:52 -0700

    axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
 import PokemonDex from './components/PokemonDex.jsx'
 import DexFormation from './components/DexFormation.jsx'
+import axios from 'axios'
 import './App.css'
 
 function App() {
   const [pokemon, setPokemon] = useState(["Squirtle", "Charmander", "Bulbasaur"])
+
+  axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+    console.log(res.data.results)
+    setPokemon(res.data.results)
+  })
+
 
   return (
     <PokemonDex pokemon = {pokemon}  />
